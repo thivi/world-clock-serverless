ab3e1c8470a46957cf6bac1041d3a17fb133077d
Thivi <theviyanthan@outlook.com>
2021-11-28 02:05:42 +0530

    Update get-time function

--- get-time/index.ts
+++ get-time/index.ts
@@ -2,8 +2,8 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const getTime: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const region = req.query.name;
-    const time = new Date().toLocaleTimeString("en-US", { timeZone: region });
+    const region = req.query.region;
+    const time = new Date().toLocaleString("en-US", { timeZone: region });
 
     context.res = {
         // status: 200, /* Defaults to 200 */
